db78e18fd5fd10a7cfa07fd93a039e9624dd096c
Trieu Bui <vantrieu.ou@gmail.com>
2020-09-17 20:22:06 +0700

    change UI

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,31 +1,33 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
+import { Form } from 'semantic-ui-react'
 import { addTodo } from '../actions'
 
-const AddTodo = ({ dispatch }) => {
+const AddTodo = ({ selectedDate, dispatch }) => {
   let input
 
-  return (
-    <div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault()
-          if (!input.value.trim()) {
-            return
-          }
-          dispatch(addTodo(input.value))
-          input.value = ''
-        }}
-      >
-        <input
-          ref={(node) => {
-            input = node
-          }}
-        />
-        <button type="submit">Add Todo</button>
-      </form>
-    </div>
+  return selectedDate ? (
+    <Form
+      onSubmit={(e) => {
+        e.preventDefault()
+        if (!input.value.trim()) {
+          return
+        }
+        dispatch(addTodo(input.value))
+        input.value = ''
+      }}
+      style={{ margin: '6px' }}
+    >
+      <Form.Group>
+        <Form.Field>
+          <input placeholder="Add Todo" ref={(node) => (input = node)} />
+        </Form.Field>
+        <Form.Button content="Submit" color="blue" />
+      </Form.Group>
+    </Form>
+  ) : (
+    <></>
   )
 }
 
-export default connect()(AddTodo)
+export default connect(({ selectedDate }) => ({ selectedDate }))(AddTodo)
--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,36 @@ import React, { PropTypes } from 'react'
 import AddTodo from './AddTodo'
 import VisibleTodoList from './VisibleTodoList'
 
+import 'semantic-ui-css/semantic.min.css'
+import { Container, Header, Segment } from 'semantic-ui-react'
+import { SingleDatePicker } from 'react-google-flight-datepicker'
+import 'react-google-flight-datepicker/dist/main.css'
+
 const App = () => (
   <div>
-    <AddTodo />
-    <VisibleTodoList />
+    <Header
+      as="h3"
+      content="Todo Calendar Simple App"
+      textAlign="center"
+      style={{ marginTop: '3em' }}
+    />
+    <Container text>
+      <Segment.Group>
+        <Segment>
+          <SingleDatePicker
+            startDate={new Date()}
+            dateFormat="YYYY-MM-DD"
+            monthFormat="MMM YYYY"
+            startWeekDay="monday"
+            highlightToday="true"
+          />
+          <AddTodo />
+        </Segment>
+        <Segment>
+          <VisibleTodoList />
+        </Segment>
+      </Segment.Group>
+    </Container>
   </div>
 )
 
