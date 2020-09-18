7b94faa2980be33dc802205b477a077dc2117872
Trieu Bui <vantrieu.ou@gmail.com>
2020-09-18 17:51:04 +0700

    useParams in Calendar comp

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import moment from 'moment'
-import { useHistory } from 'react-router-dom'
+import { useHistory, useParams } from 'react-router-dom'
 
 import { SingleDatePicker } from 'react-google-flight-datepicker'
 import 'react-google-flight-datepicker/dist/main.css'
-import { selectDate } from '../actions'
 
 export default () => {
   let history = useHistory()
+  const { selectedDate } = useParams()
+  const startDate = selectedDate ? moment(selectedDate) : new Date()
   return (
     <SingleDatePicker
-      startDate={new Date()}
+      startDate={startDate}
       dateFormat="YYYY-MM-DD"
       monthFormat="MMM YYYY"
       startWeekDay="monday"
