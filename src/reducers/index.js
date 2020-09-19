c5fe0fce5f59a0a83c81ab06bfa7d63d410286e4
Trieu Bui <vantrieu.ou@gmail.com>
2020-09-19 09:34:57 +0700

    remove selectedDate reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,15 +2,6 @@ import { combineReducers } from 'redux'
 import byId, * as fromById from './byId'
 import createTodoList, * as fromList from './createTodoList'
 
-// const selectedDate = (state = 'all', action) => {
-//   switch (action.type) {
-//     case 'SELECT_DATE':
-//       return action.date
-
-//     default:
-//       return state
-//   }
-// }
 
 const listByDate = (state = {}, action) => {
   const date = action.date || 'all'
